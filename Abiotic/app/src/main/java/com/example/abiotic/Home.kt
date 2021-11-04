ab3c7cbb3562d1030bbb12b1fde44deb53a8aac7
abcdalistan <anazel.dalistan@gmail.com>
2021-11-04 16:07:44 +0800

    Mobile Application

---
+++ Abiotic/app/src/main/java/com/example/abiotic/Home.kt
@@ -0,0 +1,25 @@
+package com.example.abiotic
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.abiotic.databinding.FragmentHomeBinding
+
+
+class Home : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater, R.layout.fragment_home, container,false)
+
+        return binding.root
+    }
+
+
+}
