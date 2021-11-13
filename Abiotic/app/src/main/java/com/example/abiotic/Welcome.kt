395a6e403cacf7f134928892c6b044c818e76d1e
NetAcad DEVASC <changeme@example.com>
2021-11-13 02:17:28 +0000

    Added Abiotic folder from Dalistan

---
+++ Abiotic/app/src/main/java/com/example/abiotic/Welcome.kt
@@ -0,0 +1,47 @@
+package com.example.abiotic
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.abiotic.databinding.FragmentWelcomeBinding
+
+
+class Welcome : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = DataBindingUtil.inflate<FragmentWelcomeBinding>(inflater, R.layout.fragment_welcome, container,false)
+
+        binding.continueBtn.setOnClickListener{ view: View ->
+            view?.findNavController().navigate(R.id.action_welcome_to_home2)
+        }
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController()) || super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity as AppCompatActivity).supportActionBar?.title = "Abiotic Stress App"
+
+    }
+
+
+}
