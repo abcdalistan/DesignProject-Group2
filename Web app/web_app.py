d64fdd4d06314d721371c65e617f647ea4c4ffca
rhwlr <rwlr167@gmail.com>
2021-11-26 18:15:40 +0800

    Login and Register page

--- Web app/web_app.py
+++ Web app/web_app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def hello():
     return render_template("web.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 @app.route("/about")
 def about():
     return render_template("team.html")
